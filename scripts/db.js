ef65680dcca44890b480a02a80b15e8737111b68
Anika Mayer <mayer.anika@protonmail.com>
2026-07-23 16:26:27 +0200

    add: dishes objects/array

--- scripts/db.js
+++ scripts/db.js
@@ -2,24 +2,28 @@ const dishes = {
     "burger": [
         {
             "name": "Veggie mushroom black burger",
+            "path": "assets/img/veggie_mushroom_black_burger.jpg",
             "price": 16.9,
             "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
             "amount": 0,
         },
         {
             "name": "All meat burger",
+            "path": "assets/img/all_meat_burger.jpg",
             "price": 15.9,
             "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
             "amount": 0,
         },
         {
             "name": "Beef red burger",
+            "path": "assets/img/beef_red_burger.jpg",
             "price": 14.9,
             "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
             "amount": 0,
         },
         {
             "name": "Big chicken burger",
+            "path": "assets/img/big_chicken_burger.jpg",
             "price": 15.9,
             "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper, ",
             "amount": 0,
@@ -28,24 +32,28 @@ const dishes = {
     "pizza": [
         {
             "name": "Pizza Margherita",
+            "path": "assets/img/pizza_margherita.jpg",
             "price": 11.9,
             "description": "Tomato Sauce, Mozzarella",
             "amount": 0,
         },
         {
             "name": "Pizza Chorizo",
+            "path": "assets/img/pizza_chorizo.jpg",
             "price": 13.9,
             "description": "Tomato slices, Mozzarella, Chorizo",
             "amount": 0,
         },
         {
             "name": "Funghi",
+            "path": "assets/img/pizza_funghi.jpg",
             "price": 12.9,
             "description": "Red onion, Olives, Button Mushrooms, Mozzarella",
             "amount": 0,
         },
         {
             "name": "Quattro Formaggi with Chicken ",
+            "path": "assets/img/pizza_quattro_formaggi_chicken.jpg",
             "price": 15.9,
             "description":
                 "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
@@ -55,6 +63,7 @@ const dishes = {
     "salad": [
         {
             "name": "Warm beef arugula salad",
+            "path": "assets/img/warm_beef_salad.jpg",
             "price": 16.9,
             "description":
                 "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
@@ -62,12 +71,14 @@ const dishes = {
         },
         {
             "name": "Mini green Salad",
+            "path": "assets/img/mini_green_salad.jpg",
             "price": 7.9,
             "description": "Green salad, Cucumber, Carrots, Parsley, Radishes ",
             "amount": 0,
         },
         {
             "name": "Green Salad with sea food",
+            "path": "assets/img/green_salad_sea_food.jpg",
             "price": 16.9,
             "description":
                 "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
@@ -75,6 +86,7 @@ const dishes = {
         },
         {
             "name": "Vegan green salad with tofu",
+            "path": "assets/img/vegan_green_salad_tofu.jpg",
             "price": 14.9,
             "description":
                 "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
