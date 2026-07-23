04520822872e1e4e660f103cbeaf3a8de2222c2b
Anika Mayer <mayer.anika@protonmail.com>
2026-07-23 09:20:14 +0200

    add: JSON array for categories

---
+++ scripts/db.js
@@ -0,0 +1,84 @@
+const dishes = {
+    "burger": [
+        {
+            "name": "Veggie mushroom black burger",
+            "price": 16.9,
+            "description": "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+            "amount": 0,
+        },
+        {
+            "name": "All meat burger",
+            "price": 15.9,
+            "description": "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ souse",
+            "amount": 0,
+        },
+        {
+            "name": "Beef red burger",
+            "price": 14.9,
+            "description": "Beef, Cheese, Tomatoes, Lettuce, Onion",
+            "amount": 0,
+        },
+        {
+            "name": "Big chicken burger",
+            "price": 15.9,
+            "description": "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper, ",
+            "amount": 0,
+        },
+    ],
+    "pizza": [
+        {
+            "name": "Pizza Margherita",
+            "price": 11.9,
+            "description": "Tomato Sauce, Mozzarella",
+            "amount": 0,
+        },
+        {
+            "name": "Pizza Chorizo",
+            "price": 13.9,
+            "description": "Tomato slices, Mozzarella, Chorizo",
+            "amount": 0,
+        },
+        {
+            "name": "Funghi",
+            "price": 12.9,
+            "description": "Red onion, Olives, Button Mushrooms, Mozzarella",
+            "amount": 0,
+        },
+        {
+            "name": "Quattro Formaggi with Chicken ",
+            "price": 15.9,
+            "description":
+                "Chicken, Mozzarella, Gorgonzola, Fontina,  Parmigiano Reggiano",
+            "amount": 0,
+        },
+    ],
+    "salad": [
+        {
+            "name": "Warm beef arugula salad",
+            "price": 16.9,
+            "description":
+                "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+            "amount": 0,
+        },
+        {
+            "name": "Mini green Salad",
+            "price": 7.9,
+            "description": "Green salad, Cucumber, Carrots, Parsley, Radishes ",
+            "amount": 0,
+        },
+        {
+            "name": "Green Salad with sea food",
+            "price": 16.9,
+            "description":
+                "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+            "amount": 0,
+        },
+        {
+            "name": "Vegan green salad with tofu",
+            "price": 14.9,
+            "description":
+                "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+            "amount": 0,
+        },
+    ],
+};
